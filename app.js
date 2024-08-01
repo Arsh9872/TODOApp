777a9b29d6f0141a49a6d5dde7100229ee769643
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-08-01 17:57:20 +0530

    importing and linking db

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connecDb = require("./db/connect");
+
 const PORT =5000;
+
 const app = express();
+
+connecDb();
 
 app.get("/",(req,res)=>{
     res.send("API home");
