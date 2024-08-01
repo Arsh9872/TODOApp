41a7dc61290634e45f38f1f1553c13f3cbf195b9
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-08-01 17:59:39 +0530

    server runs before DB

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,17 @@
 const { default: mongoose } = require("mongoose");
 
-const connecDb = ()=> {
-    mongoose
-    .connect("mongodb://localhost:27017/tododb")
-    .then((result)=>{
-        console.log("Database Connected");
-    })
-    .catch((err)=>{
-        console.error(err);
-    });
-}
+// const connecDb = ()=> {      // server runs before DB
+//     mongoose
+//     .connect("mongodb://localhost:27017/tododb");
+//     // .then((result)=>{
+//     //     console.log("Database Connected");
+//     // })
+//     // .catch((err)=>{
+//     //     console.error(err);
+//     // });
+// }
+
+
+
 
   module.exports = connecDb;
