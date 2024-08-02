6e800b006a45337df1e03439de0f56a944335563
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-08-02 17:48:26 +0530

    Promise initially

--- learnjs/test.js
+++ learnjs/test.js
@@ -1,20 +1,20 @@
-function orderPizza(callback) {
-    console.log("Preparing");
-    setTimeout(() => {
-        // console.log("Pizza prepared");
-        // pizzaIsReady();
-        callback();
-    }, 5000);
+// function orderPizza(callback) {
+//     console.log("Preparing");
+//     setTimeout(() => {
+//         // console.log("Pizza prepared");
+//         // pizzaIsReady();
+//         callback();
+//     }, 5000);
     
-}
+// }
 
 
-function pizzaIsReady() {
-    console.log("Pizza prepared");
+// function pizzaIsReady() {
+//     console.log("Pizza prepared");
     
-}
-console.log("Open Ucampus");
-orderPizza(pizzaIsReady);     // we cant use pizzaIsReady() as a function as it will stuck and produce error we have to pass the arugment as a callback
+// }
+// console.log("Open Ucampus");
+// orderPizza(pizzaIsReady);     // we cant use pizzaIsReady() as a function as it will stuck and produce error we have to pass the arugment as a callback
 
 // Callback hell or pyramid of doom      == one function will call another function and call another function and go on
 // function method1(callback) {
@@ -35,3 +35,30 @@ function getLocation(){
 }
 
 */
+
+//Promise
+
+//Promise Maker
+
+//Promise Receiver
+
+// states of promise
+
+// Pending , Resolved , Rejected
+
+function getWeather() {
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Rainy");
+        },3000);
+    });
+}
+
+getWeather().then(      // first function is runs "on acceptance"    // 2nd function "On Rejection"
+    function method1(data) {
+        console.log("OnFulfilled",data);
+    },
+    function method2(data){
+        console.log("onRejected",data);
+    }
+)
